042c741eb5707b7a8a9e0a7512368eb75c7e4364
vinicius <vinicius853o@gmail.com>
2022-08-08 18:55:32 -0300

    initial commit

---
+++ convertToBinary.dart
@@ -0,0 +1,52 @@
+class ConvertNumbers {
+  static List<int> toBinary(int number) {
+    
+    List<int> binaryNumber = [];
+    final binaryNumberConverted = number.toRadixString(2).split('');
+
+    if (binaryNumberConverted.contains('-')) {
+      binaryNumberConverted.remove('-');
+      binaryNumber = binaryNumberConverted.map((e) => int.parse(e)).toList();
+      List<int> newList = numberWithSignal(binaryNumber);      
+      return newList;
+    }
+
+    binaryNumber = binaryNumberConverted.map((e) => int.parse(e)).toList();
+    binaryNumber = shiftTo32Bits(binaryNumber);
+    return binaryNumber;
+  }
+
+  static List<int> numberWithSignal(List<int> listNumber) {
+    listNumber = shiftTo32Bits(listNumber);
+
+    for (int i = 0; i < listNumber.length; i++) {
+      listNumber[i] == 1 ? listNumber[i] = 0 : listNumber[i] = 1;
+    }   
+
+    listNumber = complementOne(listNumber);
+    return listNumber;
+  }
+
+  static List<int> complementOne(List<int> listNumber) {
+    int carryIn = 1;
+    for (int i = listNumber.length - 1; i != 0; i--) {
+      if (listNumber[i] == carryIn) {
+        listNumber[i] = 0;
+        carryIn = 1;
+      } else {
+        listNumber[i] = 1;
+        carryIn = 0;
+        break;
+      }
+    }
+    return listNumber;
+  }
+
+  static List<int> shiftTo32Bits(List<int> binaryNumber) {
+    int lenghtBinaryList = binaryNumber.length;
+    for (int i = 0; i < (32 - lenghtBinaryList); i++) {
+      binaryNumber.insert(0, 0);
+    }
+    return binaryNumber;
+  }
+}
---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'convertToBinary.dart';
+
+void main() {
+  var a = ConvertNumbers.toBinary(10);
+
+  var b = ConvertNumbers.toBinary(-10);
+
+  List<int> numbers = [0, 1, 0, 1, 0];
+  
+  print(b);
+  print(b.length);
+}
