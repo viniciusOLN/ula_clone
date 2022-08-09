75cc05012d1918309c459e6b71ddb31e32919894
vinicius <vinicius853o@gmail.com>
2022-08-09 18:52:08 -0300

    testando as funções

--- main.dart
+++ main.dart
@@ -1,8 +1,21 @@
-import 'convertToBinary.dart';
+import 'ulaController.dart';
 
 void main() {
-  var a = ConvertNumbers.toBinary(10);
+  Ula ula = Ula(sizeUla: 8);
 
-  var b = ConvertNumbers.toBinary(-10);
-  print(b);
+  List<int> andOp = ula.andOperation(10, 10);
+  List<int> orOp = ula.orOperation(10, 10);
+  List<int> addOp = ula.addOperation(11, 3);
+  List<int> subOp = ula.subOperation(13, 15);
+  int largestNumber = ula.largestNumber(-3, 10);
+  List<int> norOp = ula.norOperation(10, 10);
+  int overOp = ula.addOverflow(-90, -40);
+
+  //print(andOp);
+  //print(orOp);
+  //print(addOp);
+  // print(subOp);
+  // print(largestNumber);
+  //print(norOp);
+  print(overOp);
 }
